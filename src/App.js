0e37085d6ee51b00a12142ed3b0f7496cd57b38d
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-10-14 15:25:59 -0500

    Added bootstrap and container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import './App.css';
 import Search from "./Search";
 
-function App() {
+export default function App() {
    return (
     <div className="App">
+      <div className="container">
       <div>
         <h1>Weather App</h1>
         <p>
           <Search />
         </p>
         <footer> This project was coded by Jenny Gomez</footer>
+      </div>
       </div>
     </div>
   );
